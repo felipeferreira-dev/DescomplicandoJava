ae03137a2e789754543867bf2c203d8c36833794
Felipe <dev.felipeferreira@gmail.com>
2024-03-07 02:58:16 -0300

    Corrigindo lógica de saida do looping

--- src/br/com/devfelipebemvindo/felipe/projeto_resumao/bank/App.java
+++ src/br/com/devfelipebemvindo/felipe/projeto_resumao/bank/App.java
@@ -6,35 +6,39 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String args[]) {
-        boolean isExit = true;
+        boolean isExit = false;
         Scanner scanner = new Scanner(System.in);
         Log logger = new Log();
 
         Bank santander = new Bank("0001");
         Account account;
 
-        while (isExit) {
+        // Criando um looping para criar contas
+        while (!isExit) {
 
             logger.print("Digite -> C (Criar Conta) | SAIR (finalizar)");
-            String c = scanner.nextLine();
+            String c = scanner.nextLine(); // capturando texto
 
-            if (c.toUpperCase().equals("C")) {
-                logger.print("Digite seu nome: ");
-                String name = scanner.nextLine();
-                account = santander.generateAccount(name);
-                santander.insertAccount(account);
+            switch (c.toUpperCase()) {
+                case "C" -> {
+                    logger.print("Digite seu nome: ");
+                    String name = scanner.nextLine();
+                    account = santander.generateAccount(name);
+                    santander.insertAccount(account);
 
-                generateAccount(scanner, logger, account, isExit);
+                    generateAccount(scanner, logger, account, isExit);
 
-                scanner = new Scanner(System.in);
-            } else if (c.toUpperCase().equals("SAIR")) {
-                isExit = false;
-                break;
-            } else
-                logger.print("Comando inválido! Tente novamente.");
+                    scanner = new Scanner(System.in);
+                }
+
+                case "SAIR" -> isExit = true;
+
+                default -> logger.print("Comando inválido! Tente novamente.");
+            }
         }
 
         List<Account> listAccounts = santander.getAccounts();
+
         for (Account acount : listAccounts) {
             logger.print(acount.toString());
         }
@@ -43,7 +47,7 @@ public class App {
     }
 
     private static void generateAccount(Scanner scanner, Log logger, Account account, boolean isExit) {
-        while (isExit) {
+        while (!isExit) {
             logger.print("Digite -> D (deposito) | S (saque) | SAIR (finalizar)");
             String operation = scanner.nextLine();
 
@@ -57,7 +61,7 @@ public class App {
                     account.withDrow(scanner.nextDouble());
                 }
                 case "SAIR" -> {
-                    isExit = false;
+                    isExit = true;
                 }
                 default -> {
                     logger.print("Comando inválido!");
